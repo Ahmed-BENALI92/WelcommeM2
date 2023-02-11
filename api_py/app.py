8ab26c18fd02e5521f53b03531e15b9ae45be80e
PaulGorlicki <paul.gorlicki@gmail.com>
2023-02-11 21:32:11 +0100

    s

--- api_py/app.py
+++ api_py/app.py
@@ -20,24 +20,13 @@ import matplotlib.pyplot as plt
 import datetime as dt
 import numpy as np
 app = Flask(__name__)
-es = pd.read_csv('test.csv', sep=';')
-es = es.drop(columns=['id','horaires', 'prix_id', 'services_service', 'horaires_automate_24_24', 'epci_code', 'epci_name', 'reg_code', 'com_name', 'com_code'], axis=1)
-print(es.shape)
-print(es.dtypes)
-list(es)
-lat = []
-lon = []
-for line in es.geom:
-    geo = line.split(",")
-    lat.append(geo[0])
-    lon.append(geo[1])
+d = pd.read_csv("new_2022.csv")
 
-es["latitude"] = lat
-es["longitude"] = lon
+client = openrouteservice.Client(key='5b3ce3597851110001cf6248ef96e5f27bcc4bcd989bc39299e5060b')
 
 # partie MC
 def getPrixCarburant(date="06/02/2023", cp=75):
-    PrixCarburant = pd.read_csv("2022.csv")
+    PrixCarburant = pd.read_csv("new_2022.csv")
     data = PrixCarburant.loc[PrixCarburant['nom'] == 'Gazole']
     #Supprimer moi les data avec valeur < 1
     data = data.loc[data['valeur'] > 1]
@@ -63,8 +52,21 @@ def getPrixCarburant(date="06/02/2023", cp=75):
     Y_pred = regressor.predict(varaible_input)
     return Y_pred[0]
 
+def coordonnees():    
+    currentPosition = request.json['currentPosition']
+    destination = request.json['destination']
+    dataDepar = getGeoCode(currentPosition)
+    dataArrive = getGeoCode(destination)
+    depart = float(dataDepar['lon']), float(dataDepar['lat'])
+    arrivee =  float(dataArrive['lon']), float(dataArrive['lat'])
+    coords = ((depart, arrivee))
+    return coords
 
-# fin partie MC
+def decode(coords):
+    geometry = client.directions(coords)['routes'][0]['geometry']
+    decoded = polyline.decode(geometry)
+    return decoded
+
 def arret_plein(decoded, m):
     coords = []
     for i in range(1, len(decoded)):
@@ -75,7 +77,24 @@ def arret_plein(decoded, m):
             popup="Arrêt plein\n" + str(decoded[i]),
             icon=folium.Icon(color="blue"),
             ).add_to(m)
-    return coords  
+    if coords == []:
+        coords.append(decoded[len(decoded)-1])        
+    return coords   
+
+def station_arret(decoded, m):
+    arret = arret_plein(decoded, m)    
+    liste_arret = []
+    for i in range(0, len(arret)):
+        for j in range(0, len(d)):
+            if haversine(arret[i][0], arret[i][1], float(d.latitude[j]), float(d.longitude[j])) < 10:
+                liste_arret.append(d)
+                folium.Circle(
+                location=[float(d.latitude[j]), float(d.longitude[j])],
+                radius=10,
+                color='red',
+                fill=False).add_to(m)
+    return liste_arret            
+
 def haversine(lat1, lon1, lat2, lon2):
     R = 6371 # radius of earth in km
     dLat = math.radians(lat2 - lat1)
@@ -100,10 +119,6 @@ def getGeoCode(address):
         return None
 
 def trajetMap(lat, lon,lat2, lon2):    
-
-    #key pour utiliser le service de openrouteservice
-    client = openrouteservice.Client(key='5b3ce3597851110001cf6248ef96e5f27bcc4bcd989bc39299e5060b')
-
     #coordonnées de départ et d'arrivée du GPS
     #openrouteservice prend les coordonnées dans l'ordre (lon, lat) et non (lat, lon) comme pour folium
     #on inverse donc les coordonnées avec depart_reverse et arrivee_reverse
@@ -142,155 +157,9 @@ def trajetMap(lat, lon,lat2, lon2):
     ).add_to(m)
     #on affiche les arrêts sur la carte
     arret_plein(decoded, m)
+    station_arret(decoded, m)
     m.save('map.html')
     return m._repr_html_()
-
-@app.route('/hitmap')
-def map(): 
-    map = folium.Map(location=[48.78, 2.17], zoom_start=7, )
-
-    for i in range(0, len(es)):
-        lat = es.latitude[i]
-        lon = es.longitude[i]
-        prix = es.prix_valeur[i]
-
-        if prix < 1.0:
-            folium.Circle(
-            radius=5,
-            location=[lat, lon],
-            color='lime',
-            fill=False,).add_to(map)
-
-        elif prix < 1.5:
-            folium.Circle(
-            radius=5,
-            location=[lat, lon],
-            color='yellow',
-            fill=False,).add_to(map)
-
-        elif prix < 2.0:
-            folium.Circle(
-            radius=5,
-            location=[lat, lon],
-            color='pink',
-            fill=False,).add_to(map)
-
-        elif prix >= 2.0:
-            folium.Circle(
-            radius=5,
-            location=[lat, lon],
-            color='red',
-            fill=False,).add_to(map)
-
-    return map._repr_html_()
-
-@app.route('/graph')
-def graph():
-
-    Normandie = 0
-    Corse = 0
-    Bretagne = 0
-    Occitanie = 0
-    CVL = 0
-    GE = 0
-    NA = 0
-    ARA = 0
-    PACA = 0
-    IDF = 0
-    PDL = 0
-    HDF = 0
-    BFC = 0
-
-    labels = 'Normandie', 'Corse', 'Bretagne', 'Occitanie', 'Grand Est', 'Centre-Val de Loire', 'Nouvelle-Aquitaine', 'Auvergne-Rhône-Alpes', "PACA", 'IDF', 'Pays de la Loire', 'HDF', 'BFC'
-    for i in es.reg_name:
-        if i == 'Normandie':
-            Normandie=Normandie+1
-        if i == 'Corse':
-            Corse=Corse+1
-        if i == 'Bretagne':
-            Bretagne=Bretagne+1
-        if i == 'Occitanie':
-            Occitanie=Occitanie+1  
-        if i == 'Centre-Val de Loire':
-            CVL=CVL+1      
-        if i == 'Grand Est':
-            GE=GE+1  
-        if i == 'Nouvelle-Aquitaine':
-            NA=NA+1  
-        if i == 'Auvergne-Rhône-Alpes':
-            ARA=ARA+1  
-        if i == "Provence-Alpes-Côte d'Azur":
-            PACA=PACA+1 
-        if i == 'Île-de-France':
-            IDF=IDF+1  
-        if i == 'Pays de la Loire':
-            PDL=PDL+1  
-        if i == 'Hauts-de-France':
-            HDF=HDF+1  
-        if i == 'Bourgogne-Franche-Comté':
-            BFC=BFC+1  
-
-    sizes = [Normandie, Corse, Bretagne, Occitanie, GE, CVL, NA, ARA, PACA, IDF, PDL, HDF, BFC]
-    colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'purple', 'red', 'green', 'yellow', 'orange', 'brown', 'lime', 'pink', 'magenta']
-
-    img = io.BytesIO()
-
-    plt.pie(sizes, labels=labels, colors=colors, 
-            autopct='%1.1f%%', shadow=True, startangle=90)
-    plt.axis('equal')   
-    plt.savefig(img, format='png')
-    
-
-    img.seek(0)
-
-    plot_url = base64.b64encode(img.getvalue()).decode()
-
-    return '<img src="data:image/png;base64,{}">'.format(plot_url)
-
-@app.route('/table')
-def table():
-
-    SP98 = 0
-    SP95 = 0
-    Gazole = 0
-    E10 = 0
-    E85 = 0
-    GPLc = 0
-    nan = 0
-
-    names = ['SP98', 'SP95', 'Gazole', 'E10', 'E85', 'GPLc', "nan"]
-    for i in es.prix_nom:
-        if i == 'SP98':
-            SP98=SP98+1
-        if i == 'SP95':
-            SP95=SP95+1
-        if i == 'Gazole':
-            Gazole=Gazole+1
-        if i == 'E10':
-            E10=E10+1
-        if i == 'E85':
-            E85=E85+1
-        if i == 'GPLc':
-            GPLc=GPLc+1
-        if i == 'nan':
-            nan=nan+1  
-
-    values = [SP98, SP95, Gazole, E10, E85, GPLc, nan]
-    img = io.BytesIO()
-
-    plt.figure(figsize=(20, 3))
-
-    plt.subplot(131)
-    plt.plot(names, values)
-    plt.suptitle('Type carburant')
-
-    plt.savefig(img, format='png')
-
-    img.seek(0)
-
-    plot_url = base64.b64encode(img.getvalue()).decode()
-
-    return '<img src="data:image/png;base64,{}">'.format(plot_url)
 
 @app.route('/trajet_map', methods=['POST'])
 def trajet_map():
@@ -309,8 +178,12 @@ def distance():
     dataDepar = getGeoCode(currentPosition)
     dataArrive = getGeoCode(destination)
     cp = int(dataArrive['postal_code'][:2]) 
-    distance = haversine(float(dataDepar['lat']), float(dataDepar['lon']), float(dataArrive['lat']), float(dataArrive['lon']))
-    distance = round(distance, 2)
+    depart = float(dataDepar['lon']), float(dataDepar['lat'])
+    arrivee =  float(dataArrive['lon']), float(dataArrive['lat'])
+    coords = ((depart, arrivee))
+    #appelle de la fonction directions de openrouteservice
+    res = client.directions(coords)
+    distance = round(res['routes'][0]['summary']['distance']/1000)
     prixPrevision = getPrixCarburant(date, cp)
     prixPrevision = float(prixPrevision.item())
     prixTotal = float(prixPrevision) * float(distance)/4.7
